00bef52e49e8876afdd8fbca88f1e6acf1a3e3d3
Moxley Stratton <moxley@moxleydata.com>
2012-04-14 16:52:13 -0700

    Bookmarklet compiler

---
+++ bookmarklet-compiler.js
@@ -0,0 +1,31 @@
+/**
+ * Bookmarklet Compiler
+ * 
+ * @copyright Moxley Stratton - http://www.moxleystratton.com/
+ */
+
+window.Bookmarklet = {
+  compileBody: function (source) {
+    source = source.replace(/[\t\r\n]/g, ''); // Remove tabs, newlines
+    source = source.replace(/\/\*[^*]*\*\//, ''); // Remove multi-line comments; This needs more work
+    source = escape(source);
+    return source;
+  },
+  makeURL: function (sourceCode) {
+    return 'javascript:(function(){' + this.compileBody(sourceCode) + '}());';
+  },
+  forPost: function () {
+    var source = document.getElementById('source').value;
+    var display = document.getElementById('display');
+    var url = this.makeURL(source);
+    var container = document.getElementById('generated');
+    for (var i = 0; i < container.childNodes.length; i++) {
+      container.removeChild(container.childNodes[i]);
+    }
+    var a = document.createElement('a');
+    a.href = url;
+    var text = document.createTextNode('Bookmark this link');
+    a.appendChild(text);
+    container.appendChild(a);
+  }
+};
